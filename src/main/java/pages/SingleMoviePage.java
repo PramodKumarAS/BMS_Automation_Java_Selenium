8c758ee65dc751d7fc39ed832e47cd9d1dde9d2f
Pramod Kumar <pramodkumaras143@gmail.com>
2026-01-12 08:31:59 +0530

    Started with user e2e

---
+++ src/main/java/pages/SingleMoviePage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import base.BasePage;
+import customElements.*;
+
+public class SingleMoviePage extends BasePage<SingleMoviePage> {
+ 
+	public SingleMoviePage(WebDriver driver) {
+		super(driver);
+	}
+	
+	public Element<SingleMoviePage> ele_SingleMovieTitle() {
+		return element().setProperties(By.xpath("//*[contains(@class,'movie')]//h1"));
+	}
+	
+	public Element<SingleMoviePage> ele_Langauge(){
+		return element().setProperties(By.xpath("//*[contains(@class,'movie')]//p[normalize-space(text())='Language:']//span"));
+	}
+	
+	public Element<SingleMoviePage> ele_Genre(){
+		return element().setProperties(By.xpath("//*[contains(@class,'movie')]//p[normalize-space(text())='Genre:']//span"));
+	}
+	public Element<SingleMoviePage> ele_ReleaseDate(){
+		return element().setProperties(By.xpath("//*[contains(@class,'movie')]//p[normalize-space(text())='Release Date:']//span"));
+	}
+	public Element<SingleMoviePage> ele_Duration(){
+		return element().setProperties(By.xpath("//*[contains(@class,'movie')]//p[normalize-space(text())='Duration:']//span"));
+	}
+
+	public Input<SingleMoviePage> input_ChooseTheDate(){
+		return input().setProperties(By.xpath("//*[@type=\"date\"]"));
+	}
+	
+}
---
+++ src/test/java/tests/UserE2E.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.testng.annotations.*;
+
+import base.BaseTest;
+import pages.HomePage;
+
+public class UserE2E extends BaseTest{
+	
+	@BeforeClass
+	public void beforeClass() {
+		loginToApp();
+	}
+	
+	@Test(priority=1,testName="User Booking a show")
+	public void bookShow() {
+		
+		HomePage homePage = new HomePage(driver);
+		homePage
+		   .txt_SearchMovies().setText("Avengers: Endgame")
+		   .ele_MoviesPoster("Avengers: Endgame").click();
+	}
+}
